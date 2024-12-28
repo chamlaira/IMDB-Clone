ea37c2bee57033af56cc897fb4ee7a8260ec549a
laira <laira@whitewidget.com>
2024-12-28 14:38:02 +0800

    feat(backend): implement caching

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import axios from "axios";
 import cors from "cors";
+import Redis from "ioredis";
 
 dotenv.config();
 
 const app = express();
+const redis = new Redis();
 
 const PORT = process.env.PORT || 8080;
 const API_URL = process.env.API_URL || "http://www.omdbapi.com/";
@@ -17,31 +19,55 @@ app.get("/", (_, res) => {
   res.send(`OMDB app listening on port ${PORT}`);
 });
 
-// Search for movies
+// Search for movies.
 app.get("/search/:searchParam/page/:pageNumber", async (req, res) => {
   const searchParam = req.params.searchParam;
   const pageNumber = req.params.pageNumber;
-  // Call the OMDB API with searchParam
+  const cacheKey = `search-${searchParam}-${pageNumber}`;
+
+  // Check if the search results are in the cache.
+  const cacheData = await redis.get(cacheKey);
+  if (cacheData) {
+    res.json(JSON.parse(cacheData));
+    return;
+  }
+
+  // Call the OMDB API with searchParam.
   await axios
     .get(`${API_URL}?apiKey=${API_KEY}&s=${searchParam}&page=${pageNumber}`)
-    .then((response) => {
-      res.send(response.data);
+    .then(async (response) => {
+      res.json(response.data);
+
+      // Cache the search results for 1 hour.
+      await redis.set(cacheKey, JSON.stringify(response.data), "EX", 60 * 60);
     })
     .catch((error) => {
       console.error(error);
     });
 });
 
-// Get movie details
-app.get("/movie/:movieId", async (req, res) => {
+// Get movie details.
+app.get("/movie/:movieId", async (req: Request, res: Response): Promise<void> => {
   const movieId = req.params.movieId;
-  // Call the OMDB API with movieId
+  const cacheKey = `movie-${movieId}`;
+
+  // Check if the movie details are in the cache.
+  const cacheData = await redis.get(cacheKey);
+  if (cacheData) {
+    res.json(JSON.parse(cacheData));
+    return;
+  }
+  // Call the OMDB API with movieId.
   await axios.get(`${API_URL}?apiKey=${API_KEY}&i=${movieId}`)
-    .then((response) => {
-      res.send(response.data);
+    .then(async (response) => {
+      res.json(response.data);
+
+      // Cache the movie details for 1 hour.
+      await redis.set(cacheKey, JSON.stringify(response.data), "EX", 60 * 60);
     })
     .catch((error) => {
       console.error(error);
+      res.status(500).send("Error fetching movie details");
     });
 });
 
